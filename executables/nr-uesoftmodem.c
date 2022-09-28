fa1f9fc06c9179b3c366824087c7205bb4b09337
Melissa Elkadi <melissa@episci.com>
2022-09-28 16:06:32 -0700

    Updating Channel Modelling feature
    Previously we were not even using the
    MIMO BLER data extern variable. We now
    only define one extern variable and we
    dynamically set the BLER data based on
    the gNB antenna configuration. i.e., if
    there are two uplink antennas and two
    downlink antennas, the extern will be
    populated from the MIMO BLER curves, else
    if will be the standard SISO.

--- executables/nr-uesoftmodem.c
+++ executables/nr-uesoftmodem.c
@@ -85,6 +85,7 @@ unsigned short config_frames[4] = {2,9,11,13};
 
 #include "nr_nas_msg_sim.h"
 #include <openair1/PHY/MODULATION/nr_modulation.h>
+#include "openair2/GNB_APP/gnb_paramdef.h"
 
 extern const char *duplex_mode[];
 THREAD_STRUCT thread_struct;
@@ -158,10 +159,8 @@ uint32_t       N_RB_DL    = 106;
 uint8_t abstraction_flag=0;
 
 nr_bler_struct nr_bler_data[NR_NUM_MCS];
-nr_bler_struct nr_mimo_bler_data[NR_NUM_MCS];
 
-static void init_bler_table(void);
-static void init_mimo_bler_table(void);
+static void init_bler_table(char*);
 
 /*---------------------BMC: timespec helpers -----------------------------*/
 
@@ -389,6 +388,18 @@ void *rrc_enb_process_msg(void *notUsed) {
   return NULL;
 }
 
+static void get_channel_model_mode() {
+  paramdef_t GNBParams[]  = GNBPARAMS_DESC;
+  config_get(GNBParams, sizeof(GNBParams)/sizeof(paramdef_t), NULL);
+  int num_n1_antennas = *GNBParams[GNB_PDSCH_ANTENNAPORTS_N1_IDX].iptr;
+  int num_ul_antennas = *GNBParams[GNB_PUSCH_ANTENNAPORTS_IDX].iptr;
+
+  if (num_n1_antennas == 2 && num_ul_antennas == 2)
+    init_bler_table("NR_MIMO2x2_AWGN_RESULTS_DIR");
+  else
+    init_bler_table("NR_AWGN_RESULTS_DIR");
+}
+
 
 int main( int argc, char **argv ) {
   int set_exe_prio = 1;
@@ -467,8 +478,7 @@ int main( int argc, char **argv ) {
   PHY_vars_UE_g[0] = malloc(sizeof(PHY_VARS_NR_UE *)*MAX_NUM_CCs);
   if (get_softmodem_params()->emulate_l1) {
     RCconfig_nr_ue_macrlc();
-    init_bler_table();
-    init_mimo_bler_table();
+    get_channel_model_mode();
   }
 
   if (get_softmodem_params()->do_ra)
@@ -549,12 +559,12 @@ int main( int argc, char **argv ) {
 }
 
 // Read in each MCS file and build BLER-SINR-TB table
-static void init_bler_table(void) {
+static void init_bler_table(char *env_string) {
   memset(nr_bler_data, 0, sizeof(nr_bler_data));
 
-  const char *awgn_results_dir = getenv("NR_AWGN_RESULTS_DIR");
+  const char *awgn_results_dir = getenv(env_string);
   if (!awgn_results_dir) {
-    LOG_W(NR_MAC, "No $NR_AWGN_RESULTS_DIR\n");
+    LOG_W(NR_MAC, "No %s\n", env_string);
     return;
   }
 
@@ -597,59 +607,6 @@ static void init_bler_table(void) {
       nlines++;
     }
     nr_bler_data[i].length = nlines;
-    fclose(pFile);
-  }
-}
-
-// Read in each MCS file and build BLER-SINR-TB table
-static void init_mimo_bler_table(void) {
-  memset(nr_mimo_bler_data, 0, sizeof(nr_mimo_bler_data));
-
-  const char *awgn_results_dir = getenv("NR_MIMO2x2_AWGN_RESULTS_DIR");
-  if (!awgn_results_dir) {
-    LOG_W(NR_MAC, "No $NR_MIMO2x2_AWGN_RESULTS_DIR\n");
-    return;
-  }
-
-  for (unsigned int i = 0; i < NR_NUM_MCS; i++) {
-    char fName[1024];
-    snprintf(fName, sizeof(fName), "%s/mcs%d_cdlc_mimo2x2_dl.csv", awgn_results_dir, i);
-    FILE *pFile = fopen(fName, "r");
-    if (!pFile) {
-      LOG_E(NR_MAC, "open %s: %s\n", fName, strerror(errno));
-      continue;
-    }
-    size_t bufSize = 1024;
-    char * line = NULL;
-    char * token;
-    char * temp = NULL;
-    int nlines = 0;
-    while (getline(&line, &bufSize, pFile) > 0) {
-      if (!strncmp(line, "SNR", 3)) {
-        continue;
-      }
-
-      if (nlines > NR_NUM_SINR) {
-        LOG_E(NR_MAC, "BLER FILE ERROR - num lines greater than expected - file: %s\n", fName);
-        abort();
-      }
-
-      token = strtok_r(line, ";", &temp);
-      int ncols = 0;
-      while (token != NULL) {
-        if (ncols > NUM_BLER_COL) {
-          LOG_E(NR_MAC, "BLER FILE ERROR - num of cols greater than expected\n");
-          abort();
-        }
-
-        nr_mimo_bler_data[i].bler_table[nlines][ncols] = strtof(token, NULL);
-        ncols++;
-
-        token = strtok_r(NULL, ";", &temp);
-      }
-      nlines++;
-    }
-    nr_mimo_bler_data[i].length = nlines;
     fclose(pFile);
   }
 }
