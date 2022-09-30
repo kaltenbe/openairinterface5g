7b9f90695c57c2a1f8c6fbc755b23fe8a6116dec
Roberto Louro Magueta <rmagueta@allbesmart.pt>
2022-09-30 01:22:05 +0100

    Compute normalization channel factor

--- openair1/SIMULATION/TOOLS/random_channel.c
+++ openair1/SIMULATION/TOOLS/random_channel.c
@@ -1634,11 +1634,37 @@ int random_channel(channel_desc_t *desc, uint8_t abstraction_flag) {
   }
   bzero(acorr,desc->nb_tx*desc->nb_rx*sizeof(struct complexd));
 
+  // Compute normalization factor
+  double normalization_factor = 0.0;
+  if (desc->channel_length == 1) {
+    normalization_factor = 1.0;
+  } else {
+    double amps_tot = 0.0;
+    for (l = 0; l < desc->nb_taps; l++) {
+      amps_tot += sqrt(desc->ricean_factor * desc->amps[l]);
+    }
+    for (k = 0; k < (int)desc->channel_length; k++) {
+      double sum_s = 0.0;
+      for (l = 0; l < desc->nb_taps; l++) {
+        if ((k - (desc->delays[l] * desc->sampling_rate) - desc->channel_offset) == 0) {
+          s = 1.0;
+        } else {
+          s = sin(M_PI * (k - (desc->delays[l] * desc->sampling_rate) - desc->channel_offset)) / (M_PI * (k - (desc->delays[l] * desc->sampling_rate) - desc->channel_offset));
+        }
+        sum_s += sqrt(desc->ricean_factor * desc->amps[l]) * s;
+      }
+      normalization_factor += fabs(sum_s);
+    }
+    normalization_factor /= amps_tot;
+    normalization_factor = 1 / normalization_factor;
+  }
+
   for (i=0; i<(int)desc->nb_taps; i++) {
     for (aarx=0; aarx<desc->nb_rx; aarx++) {
       for (aatx=0; aatx<desc->nb_tx; aatx++) {
-        anew[aarx+(aatx*desc->nb_rx)].r = sqrt(desc->ricean_factor*desc->amps[i]/2) * gaussdouble(0.0,1.0);
-        anew[aarx+(aatx*desc->nb_rx)].i = sqrt(desc->ricean_factor*desc->amps[i]/2) * gaussdouble(0.0,1.0);
+
+        anew[aarx + (aatx * desc->nb_rx)].r = sqrt(desc->ricean_factor * desc->amps[i] / 2) * gaussdouble(0.0, 1.0) * normalization_factor;
+        anew[aarx + (aatx * desc->nb_rx)].i = sqrt(desc->ricean_factor * desc->amps[i] / 2) * gaussdouble(0.0, 1.0) * normalization_factor;
 
         if ((i==0) && (desc->ricean_factor != 1.0)) {
           if (desc->random_aoa==1) {
@@ -1648,10 +1674,10 @@ int random_channel(channel_desc_t *desc, uint8_t abstraction_flag) {
           // this assumes that both RX and TX have linear antenna arrays with lambda/2 antenna spacing.
           // Furhter it is assumed that the arrays are parallel to each other and that they are far enough apart so
           // that we can safely assume plane wave propagation.
-          phase.r = cos(M_PI*((aarx-aatx)*sin(desc->aoa)));
-          phase.i = sin(M_PI*((aarx-aatx)*sin(desc->aoa)));
-          anew[aarx+(aatx*desc->nb_rx)].r += phase.r * sqrt(1.0-desc->ricean_factor);
-          anew[aarx+(aatx*desc->nb_rx)].i += phase.i * sqrt(1.0-desc->ricean_factor);
+          phase.r = cos(M_PI * ((aarx - aatx) * sin(desc->aoa)));
+          phase.i = sin(M_PI * ((aarx - aatx) * sin(desc->aoa)));
+          anew[aarx + (aatx * desc->nb_rx)].r += phase.r * sqrt(1.0 - desc->ricean_factor) * normalization_factor;
+          anew[aarx + (aatx * desc->nb_rx)].i += phase.i * sqrt(1.0 - desc->ricean_factor) * normalization_factor;
         }
 
 #ifdef DEBUG_CH
@@ -1672,6 +1698,7 @@ int random_channel(channel_desc_t *desc, uint8_t abstraction_flag) {
     */
     //apply correlation matrix
     //compute acorr = R_sqrt[i] * anew
+    bzero(acorr, desc->nb_tx * desc->nb_rx * sizeof(struct complexd));
     if (desc->modelid >= TDL_A && desc->modelid <= TDL_E) {
       for (aatx = 0; aatx < desc->nb_tx; aatx++) {
         for (aarx=0; aarx<desc->nb_rx; aarx++) {
@@ -1684,7 +1711,6 @@ int random_channel(channel_desc_t *desc, uint8_t abstraction_flag) {
         }
       }
     } else {
-      bzero(acorr,desc->nb_tx*desc->nb_rx*sizeof(struct complexd));
       cblas_zaxpy(desc->nb_tx*desc->nb_rx, (void *) desc->R_sqrt[i/3], (void *) anew, 1, (void *) acorr, 1);
     }
 
